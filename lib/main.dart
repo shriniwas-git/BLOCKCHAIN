cdf7159a855f7f3fa47e83d719b37453ad0d29a7
shriniwas-git <shriniwas.10c@gmail.com>
2023-12-12 20:39:50 +0530

    JBJU
    PUB

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Pages/splash_screen.dart';
+import 'Utils/routes.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black, statusBarIconBrightness: Brightness.light));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Removed GoogleFonts.poppinsTextTheme
+        textTheme: Theme.of(context).textTheme,
+      ),
+      home: const Splash2(),
+      routes: MyRoutes.routes,
+    );
+  }
+}
